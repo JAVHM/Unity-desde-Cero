a3017c057cf988a3c9a52640efb1cc17992a9f6d
JAVHM <55992367+JAVHM@users.noreply.github.com>
2020-05-13 20:35:08 -0500

    Add files via upload

---
+++ Capitulo 4/PlayerController2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+
+    Vector2 movement;
+
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkControls();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    public void checkControls()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+}
+
