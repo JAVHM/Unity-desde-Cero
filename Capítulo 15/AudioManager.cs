bfcd1ec31c10cec8f4bf85b163f5c8f3b0e5f555
JAVHM <55992367+JAVHM@users.noreply.github.com>
2020-06-23 15:09:12 -0500

    Add files via upload

---
+++ Capítulo 15/AudioManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System;
+//Agregar
+using UnityEngine.Audio;
+
+
+public class AudioManager : MonoBehaviour
+{
+    //Creamos nuestra lista de sonidos
+    public Sound[] sounds;
+
+    //Con instance vamos a asegurarnos de que solo haya un AudioManager
+    public static AudioManager instance;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        //Con instance vamos a asegurarnos de que solo haya un AudioManager
+        if (instance == null)
+            instance = this;
+        //Con esto el objeto no se destruye al pasar de escena o el reinicio de la misma
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+
+        //Sound, definimos que nos referimos a un sonido
+        //"s" es el sonido
+        //la lista de sonidos "s" es sounds
+        //Para cada sonido en la lista de sonidos....
+        foreach (Sound s in sounds)
+        {
+            //Estas son las cosas que son necesarias para cada sonido
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("BackGroundMusic");
+    }
+
+    float newVol = 1;
+    float newPitch = 1;
+    void Update()
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == "BackGroundMusic");
+        s.source.volume = newVol;
+        s.source.pitch = newPitch;
+    }
+
+    public void Play(string name)
+    {
+        //En la lista de sonidos ("Sound") está "s", se le va agragar a sus componentes estás variables
+        //1. Le decimos a que lista nos referimos
+        //2. Ahora que en esa lista lo busque por su nombre
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        //Si no se encuentra el sonido retornar nulo
+        if (s == null)
+        {
+            Debug.LogError("No se encontró el audio!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    //Creamos una clase dinamica en la cual el valor del temporal "temp" varía según el Slider
+    public void SetBGVolume(float temp)
+    {
+        newVol = temp;
+    }
+
+    public void SetBGPitch(float temp)
+    {
+        newPitch = temp;
+    }
+}
---
+++ Capítulo 15/Sound.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+//Con esto podemos crear una lista de sonidos
+[System.Serializable]
+
+//Acá ponemos todos los componentes que va a tener nuestro audio
+public class Sound
+{
+    public string name;
+
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    //Tono
+    [Range(0f, 3f)]
+    public float pitch = 1;
+
+    //Que se repita en bucle
+    public bool loop;
+
+    //Para objetos 3D en los cuales quieres que el sonido salga de un objeto
+    //Mientras más cercas estes será más claro
+
+    [HideInInspector]
+    public AudioSource source;
+}
