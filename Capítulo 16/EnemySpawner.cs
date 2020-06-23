694499296858c2708a7bd3b345cfdbe2b31999c6
JAVHM <55992367+JAVHM@users.noreply.github.com>
2020-06-23 15:10:52 -0500

    Add files via upload

---
+++ Capítulo 16/EnemySpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    //Objeto a spawnear
+    public GameObject EnemyShip;
+    float randx;
+
+    //Donde spawneará. En este caso será en un valor aleatorio de X
+    Vector2 whereToSpawn;
+    public float spawnRate = 1f;
+    float nextSpawn = 0.0f;
+    public int necessaryScore;
+
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( necessaryScore <= Score.score )
+        {
+            if (Time.time > nextSpawn)
+            {
+                //El siguiente ibjeto a Spwanear va  a cumplir esta condición: 
+                nextSpawn = Time.time + spawnRate;
+                randx = Random.Range(-9f, 9f);
+                whereToSpawn = new Vector2(randx, transform.position.y);
+                //Instanciamos el objeto nave enemiga, le decimos donde spawnear, Quaternion.identity hace que la rotación no afecte
+                Instantiate(EnemyShip, whereToSpawn, Quaternion.identity);
+            }
+        }
+        
+    }
+}
